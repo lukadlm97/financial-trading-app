eb2723c28f60715bca102d711e791522e9b28d0c
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-07 01:19:40 +0200

    setup mvvm on front with home and portfolio views

---
+++ financial-app/SimpleTrader.WPF/Commands/UpdateCurrentVewModelCommand.cs
@@ -0,0 +1,45 @@
+﻿using SimpleTrader.WPF.State.Navigators;
+using SimpleTrader.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.Commands
+{
+    public class UpdateCurrentVewModelCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private INavigator _navigator;
+
+        public UpdateCurrentVewModelCommand(INavigator navigator)
+        {
+            _navigator = navigator;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(parameter is ViewType)
+            {
+                ViewType viewType = (ViewType)parameter;
+                switch (viewType)
+                {
+                    case ViewType.Home:
+                        _navigator.CurrentViewModel = new HomeViewModel();
+                        break;
+                    case ViewType.Portfolio:
+                        _navigator.CurrentViewModel = new PortfolioViewModel();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ financial-app/SimpleTrader.WPF/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.WPF.ViewModels
+{
+    class HomeViewModel:ViewModelBase
+    {
+    }
+}
