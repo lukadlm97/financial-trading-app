7a8a251fc7adfb2ff9080b0a28c9a0f1d1a28d6f
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-08 00:06:23 +0200

    completly implemented major indexes service and view model for them

--- financial-app/SimpleTrader.WPF/ViewModels/HomeViewModel.cs
+++ financial-app/SimpleTrader.WPF/ViewModels/HomeViewModel.cs
@@ -6,5 +6,12 @@ namespace SimpleTrader.WPF.ViewModels
 {
     class HomeViewModel:ViewModelBase
     {
+        public MajorIndexViewModel MajorIndexViewModel { get; set; }
+
+        public HomeViewModel(MajorIndexViewModel majorIndexViewModel)
+        {
+            MajorIndexViewModel = majorIndexViewModel;
+        }
+
     }
 }
