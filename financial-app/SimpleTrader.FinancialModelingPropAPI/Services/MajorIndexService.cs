7172470669e83d6335894a3d5a563ede9f9ab0ba
Luka Radovanovic <lukadlm97@gmail.com>
2020-05-07 22:54:11 +0200

    refactored major index service for connection to service for all majors

--- financial-app/SimpleTrader.FinancialModelingPropAPI/Services/MajorIndexService.cs
+++ financial-app/SimpleTrader.FinancialModelingPropAPI/Services/MajorIndexService.cs
@@ -15,11 +15,30 @@ namespace SimpleTrader.FinancialModelingPropAPI.Services
         {
             using(HttpClient client = new HttpClient())
             {
-                HttpResponseMessage httpResponse = await client.GetAsync("https://financialmodelingprep.com/api/v3/majors-indexes/.DJI");
+                string uri = "https://financialmodelingprep.com/api/v3/majors-indexes/" + GetUriSuffix(indexType);
+                
+                HttpResponseMessage httpResponse = await client.GetAsync(uri);
                 string jsonResponse = await httpResponse.Content.ReadAsStringAsync();
 
                 MajorIndex majorIndex = JsonConvert.DeserializeObject<MajorIndex>(jsonResponse);
+                majorIndex.Type = indexType;
+
                 return majorIndex;
+            }
+        }
+
+        private string GetUriSuffix(MajorIndexType indexType)
+        {
+            switch (indexType)
+            {
+                case MajorIndexType.DowJones:
+                    return ".DJI";
+                case MajorIndexType.Nasdaq:
+                    return ".IXIC";
+                case MajorIndexType.SP500:
+                    return ".INX";
+                default:
+                    return ".DJI";
             }
         }
     }
